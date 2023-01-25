8df48af778df1daf941633215c0c86d8c2ef517d
Georgii Troshin <gtroshin@live.com>
2023-01-25 20:38:51 +0100

    Use another compiler.libcxx

--- build.sh
+++ build.sh
@@ -6,9 +6,9 @@ set -x
 mkdir build && cd build
 
 # since we are running the modern distributive and gcc >= 5, we need to change compiler.libcxx in default profile
-conan profile update settings.compiler.libcxx=libstdc++11 default
+# conan profile update settings.compiler.libcxx=libstdc++11 default
 
-conan install .. --build=missing
+conan install .. --build=missing -s compiler.libcxx=libstdc++11
 cmake .. -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release
 cmake --build .
 
