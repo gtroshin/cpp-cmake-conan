e8512eae28e8892d542082faa64fd4aec4a93d53
Georgii Troshin <gtroshin@live.com>
2023-01-25 20:33:49 +0100

    Use another compiler.libcxx and latest poco

--- build.sh
+++ build.sh
@@ -5,6 +5,9 @@ set -x
 
 mkdir build && cd build
 
+# since we are running the modern distributive and gcc >= 5, we need to change compiler.libcxx in default profile
+conan profile update settings.compiler.libcxx=libstdc++11 default
+
 conan install .. --build=missing
 cmake .. -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release
 cmake --build .
