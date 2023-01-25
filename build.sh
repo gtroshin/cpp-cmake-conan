e15052ce26a27c142eb9ebb0a9ed0f3ac9d9c95b
Georgii Troshin <gtroshin@live.com>
2023-01-25 20:41:50 +0100

    Add macos-latest to the build matrix

--- build.sh
+++ build.sh
@@ -5,9 +5,7 @@ set -x
 
 mkdir build && cd build
 
-# since we are running the modern distributive and gcc >= 5, we need to change compiler.libcxx in default profile
-# conan profile update settings.compiler.libcxx=libstdc++11 default
-
+# since we are running the modern distributive and gcc >= 5, we need to overrride compiler.libcxx in default settings
 conan install .. --build=missing -s compiler.libcxx=libstdc++11
 cmake .. -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release
 cmake --build .
