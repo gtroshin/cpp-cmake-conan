d0fdea03d257866992b7ae68b0877566c595a839
Georgii Troshin <gtroshin@live.com>
2023-01-25 20:52:48 +0100

    Conditionally override compiler.libcxx

--- build.sh
+++ build.sh
@@ -5,12 +5,12 @@ set -x
 
 mkdir build && cd build
 
-if [[ ${{ matrix.os }} = ubuntu-latest ]]
+if [ ${{ matrix.os }} = ubuntu-latest ]
 then
-  # since we are running the modern distributive and gcc >= 5, we need to override compiler.libcxx in the default settings
-  conan install .. --build=missing -s compiler.libcxx=libstdc++11
-elif [ ${{ matrix.os }} = macos-latest ]]
-  conan install .. --build=missing -s compiler.libcxx=libstdc++
+    # since we are running the modern distributive and gcc >= 5, we need to override compiler.libcxx in the default settings
+    conan install .. --build=missing -s compiler.libcxx=libstdc++11
+elif [ ${{ matrix.os }} = macos-latest ]
+    conan install .. --build=missing -s compiler.libcxx=libstdc++
 fi
 
 
