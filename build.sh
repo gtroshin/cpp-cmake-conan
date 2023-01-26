497d01a1b69cf656c4bd4642a32fdd5a60e94a5b
Georgii Troshin <gtroshin@live.com>
2023-01-26 11:31:02 +0100

    Add unit tests

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ if [ "$RUNNER_OS" == "Linux" ]; then
     # Since we are running the modern Linux distributive and gcc >= 5, we need to override compiler.libcxx in the default settings
     conan install .. --build=missing -s compiler.libcxx=libstdc++11
 elif [ "$RUNNER_OS" == "macOS" ]; then
-    conan install .. --build=missing -s compiler.libcxx=libstdc++
+    conan install .. --build=missing
 else
     echo "$RUNNER_OS is not supported by build script"
     exit 1
