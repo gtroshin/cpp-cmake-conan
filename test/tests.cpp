f69bbda9ddf4114d7c4e36cd0dd4324d91e3d561
Georgii Troshin <gtroshin@live.com>
2023-01-26 13:01:21 +0100

    Simplify tests

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,13 +1,6 @@
-#include <catch2/catch.hpp>
-
-unsigned int Factorial(unsigned int number) // NOLINT(misc-no-recursion)
+#include <assert.h>
+int main()
 {
-  return number <= 1 ? number : Factorial(number - 1) * number;
-}
-
-TEST_CASE("Factorials are computed", "[factorial]") {
-  REQUIRE(Factorial(1) == 1);
-  REQUIRE(Factorial(2) == 2);
-  REQUIRE(Factorial(3) == 6);
-  REQUIRE(Factorial(10) == 3628800);
+    assert(1 != 2);
+    return 0;
 }
