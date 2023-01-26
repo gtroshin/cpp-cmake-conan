6ce89cbbec9ce122ac6daadd85e2f27d868cf548
Georgii Troshin <gtroshin@live.com>
2023-01-26 14:49:53 +0100

    Use gtest to run tests

--- build.sh
+++ build.sh
@@ -7,9 +7,9 @@ mkdir build && cd build
 
 if [ "$RUNNER_OS" == "Linux" ]; then
     # Since we are running the modern Linux distributive and gcc >= 5, we need to override compiler.libcxx in the default settings
-    conan install .. --build=missing -s compiler.libcxx=libstdc++11
+    conan install -s build_type=Debug .. --build=missing -s compiler.libcxx=libstdc++11
 elif [ "$RUNNER_OS" == "macOS" ]; then
-    conan install .. --build=missing
+    conan install -s build_type=Debug .. --build=missing
 else
     echo "The $RUNNER_OS is not supported by build script"
     exit 1
